cf8e4c269222074cd764a8c9342cc96a8c25ed58
Monjur Morshed <xavieritsolution@gmail.com>
2025-12-26 08:39:17 +0600

    React Project First push after config
    React Project First push after config with vite, material ui, router and creating pages

---
+++ React.js/Intro to React.Js/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes,Route } from "react-router-dom";
+import './App.css';
+import Home from "./pages/homepage/home";
+import Services from "./pages/services/services";
+import Teams from "./pages/teams/teams";
+import Leagues from "./pages/leagues/leagues";
+import Contact from "./pages/contact/contact";
+function App() {
+  
+  return (
+    <Routes>
+      <Route path='/' element= {<Home />}/>
+      <Route path='/services' element= {<Services />}/>
+      <Route path='/teams' element= {<Teams />}/>
+      <Route path='/leagues' element= {<Leagues />}/>
+      <Route path='/contacts' element= {<Contact />}/>
+    </Routes>
+  )
+}
+
+export default App;
