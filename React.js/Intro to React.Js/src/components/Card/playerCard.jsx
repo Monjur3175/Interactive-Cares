aef0b0737c25abead3486779b0e54287b2377471
Monjur Morshed <137275524+Monjur3175@users.noreply.github.com>
2026-01-22 23:05:47 +0600

    Final Code Push,Project Done

--- React.js/Intro to React.Js/src/components/Card/playerCard.jsx
+++ React.js/Intro to React.Js/src/components/Card/playerCard.jsx
@@ -1,4 +1,11 @@
-import { Card, CardContent, CardHeader, CardMedia, Grid, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 export default function playerCard({ player }) {
@@ -7,7 +14,7 @@ export default function playerCard({ player }) {
       <Card sx={{ maxWidth: 345, height: 800 }}>
         <CardHeader
           title={player.strPlayer}
-          subheader={`${player.dateBorn},${player.strBirthLocation}`}
+          subheader={`${player.dateBorn},${player.strBirthLocation}-${player.strEthnicity},${player.strNationality}`}
         />
         <CardMedia
           component="img"
@@ -15,6 +22,7 @@ export default function playerCard({ player }) {
           alt={player.strPlayer}
         />
         <CardContent sx={{ height: 200, overflow: "auto" }}>
+          <Typography variant="h6">{player.strPosition}</Typography>
           <Typography variant="body2" sx={{ color: "text.secondary" }}>
             {player.strDescriptionEN}
           </Typography>
